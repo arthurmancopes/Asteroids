818436f1ca1b768291efb4d864d4f01677bbcff2
arthurmancopes <arthur.msiq@gmail.com>
2025-09-30 17:36:32 -0300

    C: Create the player sprite and the hitbox

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 def main():
     pygame.init()
     print("Starting Asteroids!")
@@ -7,12 +9,19 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    clock = pygame.time.Clock()
+    dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Instantiate Player
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))  # Fill screen with black
-        pygame.display.flip()   # Refresh the screen
+        player.draw(screen)      # Draw the player each frame
+        pygame.display.flip()    # Refresh the screen
+        dt = clock.tick(60)/1000 # Pause until 1/60th of a second has passed
 
 if __name__ == "__main__":
     main()
